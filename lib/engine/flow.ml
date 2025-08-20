7d11219b41aa7b5963bbd3328e0b40527e13c91d
gr-im <grimfw@gmail.com>
2025-08-20 05:42:36 +0200

    More combinators for Flow

--- lib/engine/flow.ml
+++ lib/engine/flow.ml
@@ -4,13 +4,31 @@ type ('a, 'b, 'handler) t =
   ; dynamic_deps : bool
   }
 
-let make ?(dynamic_deps = true) ?(deps = Deps.empty) action =
+let eff ?(dynamic_deps = true) ?(deps = Deps.empty) action =
   { action; dynamic_deps; deps }
 ;;
 
+let lift ?(dynamic_deps = false) ?deps f =
+  eff ~dynamic_deps ?deps (fun x -> Eff.return (f x))
+;;
+
+let id = { action = Eff.return; deps = Deps.empty; dynamic_deps = false }
 let action { action; _ } = action
 let deps { deps; _ } = deps
 let has_dynamic_deps { dynamic_deps; _ } = dynamic_deps
-let set_dynamic_deps dynamic_deps flow = { flow with dynamic_deps }
+let on_dynamic_deps f flow = { flow with dynamic_deps = f flow.dynamic_deps }
+let on_deps f flow = { flow with deps = f flow.deps }
+
+let set_dynamic_deps dynamic_deps flow =
+  on_dynamic_deps (fun _ -> dynamic_deps) flow
+;;
+
 let with_dynamic_deps flow = set_dynamic_deps true flow
 let no_dynamic_deps flow = set_dynamic_deps false flow
+
+let dimap f g flow =
+  { flow with action = (fun x -> Eff.map g (action flow (f x))) }
+;;
+
+let lmap f flow = dimap f (fun x -> x) flow
+let rmap f flow = dimap (fun x -> x) f flow
--- lib/engine/flow.mli
+++ lib/engine/flow.mli
@@ -1,14 +1,37 @@
 type ('a, 'b, 'handler) t
 
-val make
+val eff
   :  ?dynamic_deps:bool
   -> ?deps:Deps.t
   -> ('a -> ('b, 'handler) Eff.t)
   -> ('a, 'b, 'handler) t
 
+val lift
+  :  ?dynamic_deps:bool
+  -> ?deps:Deps.t
+  -> ('a -> 'b)
+  -> ('a, 'b, 'handler) t
+
+val id : ('a, 'a, 'handler) t
 val action : ('a, 'b, 'handler) t -> 'a -> ('b, 'handler) Eff.t
 val deps : ('a, 'b, 'handler) t -> Deps.t
 val has_dynamic_deps : ('a, 'b, 'handler) t -> bool
 val set_dynamic_deps : bool -> ('a, 'b, 'handler) t -> ('a, 'b, 'handler) t
 val with_dynamic_deps : ('a, 'b, 'handler) t -> ('a, 'b, 'handler) t
 val no_dynamic_deps : ('a, 'b, 'handler) t -> ('a, 'b, 'handler) t
+
+val on_dynamic_deps
+  :  (bool -> bool)
+  -> ('a, 'b, 'handler) t
+  -> ('a, 'b, 'handler) t
+
+val on_deps : (Deps.t -> Deps.t) -> ('a, 'b, 'handler) t -> ('a, 'b, 'handler) t
+
+val dimap
+  :  ('a -> 'b)
+  -> ('c -> 'd)
+  -> ('b, 'c, 'handler) t
+  -> ('a, 'd, 'handler) t
+
+val lmap : ('a -> 'b) -> ('b, 'c, 'handler) t -> ('a, 'c, 'handler) t
+val rmap : ('a -> 'b) -> ('c, 'a, 'handler) t -> ('c, 'b, 'handler) t
